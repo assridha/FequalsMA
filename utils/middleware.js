8126d41afba37f775b355d20f89336c9bc9509f7
Ashwin Sridhar <ashwin.sridhar.a@gmail.com>
2023-04-15 10:27:01 -0400

    added isAdmin function

--- utils/middleware.js
+++ utils/middleware.js
@@ -7,3 +7,13 @@ module.exports.isLoggedIn = (req, res, next) => {
 }
 
 
+// function to check if user is Admin by comparing with the admin username in the .env file and the user id of the current user
+module.exports.isAdmin = (req, res, next) => {
+    if (req.user._id.toString() !== process.env.ADMIN_OID) {
+        req.flash('error', 'you must be an admin to do that');
+        return res.redirect('back');
+    }
+    next();
+}
+
+
